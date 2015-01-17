2991a9611b79a76d366998dc427b789de51a0bcd
JustinChee <JustinChee@192.168.1.85>
2015-01-17 01:10:06 -0800

    Added Controllers and Views. Signed-off-by:JustinChee <JustinChee@192.168.1.85>

--- application/controllers/Dives.php
+++ application/controllers/Dives.php
@@ -2,10 +2,10 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Dives extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('welcome');
+        $this->load->view('dives');
     }
 }
